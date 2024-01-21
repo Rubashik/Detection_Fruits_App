a287ff910227da966103c8f972082c58d46f19bb
Rubashik <vanuaruban130303@gmail.com>
2024-01-21 17:02:44 +0200

    Changed model

--- app/src/main/java/com/example/detectionapp/Recognizer/ObjectDetectorClass.java
+++ app/src/main/java/com/example/detectionapp/Recognizer/ObjectDetectorClass.java
@@ -141,7 +141,6 @@ public class ObjectDetectorClass {
         // Before watching this video please watch my previous 2 video of
         //      1. Loading tensorflow lite model
         //      2. Predicting object
-        // In this video we will draw boxes and label it with it's name
 
         Object value=output_map.get(0);
         Object Object_class=output_map.get(1);
