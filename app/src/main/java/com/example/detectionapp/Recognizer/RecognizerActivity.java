d6fdd40ca5601617d13f3dbdac4d03ad07426869
Rubashik <vanuaruban130303@gmail.com>
2024-01-21 16:43:32 +0200

    Changed model

---
+++ app/src/main/java/com/example/detectionapp/Recognizer/RecognizerActivity.java
@@ -0,0 +1,78 @@
+package com.example.detectionapp.Recognizer;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.detectionapp.R;
+
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RecognizerActivity extends CameraActivity{
+
+
+    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognizer);
+
+        getPermission();
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                return inputFrame.rgba();
+            }
+        });
+
+
+        if(OpenCVLoader.initDebug()){
+            cameraBridgeViewBase.enableView();
+        }
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    void getPermission(){
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+}
